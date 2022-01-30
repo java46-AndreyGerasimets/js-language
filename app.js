3dfa3aef721e582c93adee708528a3885694763f
ferretvl <andrey20k@gmail.com>
2022-01-30 15:23:22 +0200

    HW15 Additional task definition

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-function displayOccurrences(array) {
-    const res = checkAndCount(array);
-    Object.entries(res).sort((e1, e2) => {
-            const res = e2[1] - e1[1];
-            return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+function createAddress (city, street) {
+    return {city, street}
 }
-
-// task 1
-function checkAndCount(array) {
-    return array.reduce(function (acc, curr) {
-        return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-      }, {});
+function createPerson(id, name, address) {
+    return {id, name, address}
 }
+const persons = [
+    createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+    createPerson(124, "Olya",  createAddress("Rehovot", "Pr.Plaut")),
+    createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+    createPerson(126, "Sara",  createAddress("Lod", "Sokolov"))
+] 
 
-// task 2
-function countBy(array, callbackFun) {
-    displayOccurrences(array.map(e => callbackFun(e)));
-}
-
-// tests
-const ar = ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
-displayOccurrences(ar);
-
-console.log('Statistics for doubles');
-const arrDouble = [6.4, 7.3, 6.5, 6.9];
-countBy(arrDouble, element => Math.floor(element));
-
-console.log('Statistics for strings');
-const arrString = ['abcd', 'lmnr', 'ab', 'dddd'];
-countBy(arrString, element => element.length);
-
-console.log('Statistics for objects');
-const arrObject = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
-            {age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ];
-countBy(arrObject, element => element.age);
+// -------------------------------
+// Write a functinon groupBy(array, .......), which has 
+// some callback parameters and performs the following actions
+// 1) breaks the array elements into groups by specified criteria
+// 2) for each group performs specified reduction operation
+// The same function would be able to show the following information 
+// when called with different callback parameters:
+// Example 1: { Rehovot: 2, 'Tel-Aviv': 1, Lod: 1 }
+// Example 2: { 
+//    Rehovot: [ 'Vasya', 'Olya' ],
+//    'Tel-Aviv': [ 'Tolya' ],
+//    Lod: [ 'Sara' ]
+// }
+//
+// Your tasks are:
+// - Decide which parameters must have such function
+// - Implement the function
+// - Implement the calls of this function implementing results of Example 1 and Example 2
