6bdbcb16ac174ca5be4e88784eb065c988a3cce3
ferretvl <andrey20k@gmail.com>
2022-01-30 20:44:11 +0200

    Finishes HW15 Additional task

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-function createAddress (city, street) {
-    return {city, street}
+function createAddress(city, street) {
+    return { city, street }
 }
 function createPerson(id, name, address) {
-    return {id, name, address}
+    return { id, name, address }
 }
 const persons = [
     createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
-    createPerson(124, "Olya",  createAddress("Rehovot", "Pr.Plaut")),
+    createPerson(124, "Olya", createAddress("Rehovot", "Pr.Plaut")),
     createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
-    createPerson(126, "Sara",  createAddress("Lod", "Sokolov"))
-] 
+    createPerson(126, "Sara", createAddress("Lod", "Sokolov"))
+];
 
-// -------------------------------
-// Write a functinon groupBy(array, .......), which has 
-// some callback parameters and performs the following actions
-// 1) breaks the array elements into groups by specified criteria
-// 2) for each group performs specified reduction operation
-// The same function would be able to show the following information 
-// when called with different callback parameters:
-// Example 1: { Rehovot: 2, 'Tel-Aviv': 1, Lod: 1 }
-// Example 2: { 
-//    Rehovot: [ 'Vasya', 'Olya' ],
-//    'Tel-Aviv': [ 'Tolya' ],
-//    Lod: [ 'Sara' ]
-// }
-//
-// Your tasks are:
-// - Decide which parameters must have such function
-// - Implement the function
-// - Implement the calls of this function implementing results of Example 1 and Example 2
+function groupBy(array, callbackFunc) {
+
+    let res = array.reduce(callbackFunc, {});
+
+    Object.entries(res).forEach(e => console.log(`${e[0]}: ${e[1]}`));
+}
+
+groupBy(persons, callbackCount);
+console.log();
+groupBy(persons, callbackList);
+
+function callbackList(acc, curr) {
+    return acc[curr.address.city] ?
+            acc[curr.address.city] = acc[curr.address.city].concat(curr.name) :  
+            acc[curr.address.city] = [].concat(curr.name), acc
+}
+
+function callbackCount(acc, curr) {
+    return acc[curr.address.city] ?
+            acc[curr.address.city] = ++acc[curr.address.city] :  
+            acc[curr.address.city] = 1, acc
+}
