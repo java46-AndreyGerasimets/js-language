0248d617e3b851e9798a306f140f861d5f44ab98
ferretvl <andrey20k@gmail.com>
2022-01-30 11:16:45 +0200

    HW13

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-// Task 1
-
-const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
-arHw.sort((a, b) => {return a % 2 == 0 ? (b % 2 == 0 ? a - b : -1) : (b % 2 == 0 ? 1 : b - a);});
-console.log(arHw);
-
-
-// Task 2
-
-const matrix = [[1, 2], [3, 4], [5, 6]];
-
-console.log(matrixTransp(matrix));
-
-function matrixTransp(matrix) {
-    return Object.keys(matrix[0]).map(colNumber => matrix.map(rowNumber => rowNumber[colNumber]));
+function myForEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+            callback(array[i], i, array);
+    }
+}
+function myMap(array, callback) {
+    const res = [];
+    function forEachCall(n, i, a) {
+            res.push(callback(n, i, a));
+    }
+    myForEach(array, forEachCall);
+    return res;
 }
